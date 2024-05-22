e97a18baa46d93254bb0e218e62cfde93b015980
Natalia <sh.nat.vlad@gmail.com>
2024-05-22 14:58:45 +0500

    feat: add keystroke validation inputs in StateLogin

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,8 +7,8 @@ export default function Signup() {
         const data = Object.fromEntries(fd.entries());
         data.acquisition = acquisitionChannel;
 
-        // reset the inputs
-        event.target.reset();
+        // // reset the inputs
+        // event.target.reset();
     }
 
     return (
